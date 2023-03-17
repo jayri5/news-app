bebe830208e8dc3e8c2b10cc019e183f6f4e7b22
Jayri Bagchi <jayribagchi@gmail.com>
2023-03-17 10:52:15 +0530

    Update news.dart

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -1,25 +1,23 @@
 import 'package:http/http.dart' as http;
-import 'package:news_app_api/models/article.dart';
+import 'package:helloworld/models/article.dart';
 import 'dart:convert';
 
-import 'package:news_app_api/secret.dart';
+import 'package:helloworld/secret.dart';
 
 class News {
+  List<Article> news = [];
 
-  List<Article> news  = [];
-
-  Future<void> getNews() async{
-
-    String url = "http://newsapi.org/v2/top-headlines?country=in&excludeDomains=stackoverflow.com&sortBy=publishedAt&language=en&apiKey=${apiKey}";
+  Future<void> getNews() async {
+    String url =
+        "http://newsapi.org/v2/top-headlines?country=in&excludeDomains=stackoverflow.com&sortBy=publishedAt&language=en&apiKey=$apiKey";
 
     var response = await http.get(url);
 
     var jsonData = jsonDecode(response.body);
 
-    if(jsonData['status'] == "ok"){
-      jsonData["articles"].forEach((element){
-
-        if(element['urlToImage'] != null && element['description'] != null){
+    if (jsonData['status'] == "ok") {
+      jsonData["articles"].forEach((element) {
+        if (element['urlToImage'] != null && element['description'] != null) {
           Article article = Article(
             title: element['title'],
             author: element['author'],
@@ -31,34 +29,26 @@ class News {
           );
           news.add(article);
         }
-
       });
     }
-
-
   }
-
-
 }
-
 
 class NewsForCategorie {
+  List<Article> news = [];
 
-  List<Article> news  = [];
-
-  Future<void> getNewsForCategory(String category) async{
-
+  Future<void> getNewsForCategory(String category) async {
     /*String url = "http://newsapi.org/v2/everything?q=$category&apiKey=${apiKey}";*/
-    String url = "http://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=${apiKey}";
+    String url =
+        "http://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=${apiKey}";
 
     var response = await http.get(url);
 
     var jsonData = jsonDecode(response.body);
 
-    if(jsonData['status'] == "ok"){
-      jsonData["articles"].forEach((element){
-
-        if(element['urlToImage'] != null && element['description'] != null){
+    if (jsonData['status'] == "ok") {
+      jsonData["articles"].forEach((element) {
+        if (element['urlToImage'] != null && element['description'] != null) {
           Article article = Article(
             title: element['title'],
             author: element['author'],
@@ -70,14 +60,7 @@ class NewsForCategorie {
           );
           news.add(article);
         }
-
       });
     }
-
-
   }
-
-
 }
-
-
